5bb9a2490f03dfa594078e490eb68bf06b740889
es94221 <104841907+es94221@users.noreply.github.com>
2026-05-15 20:46:00 +0200

    Update trip-data.js

--- trip-data.js
+++ trip-data.js
@@ -15,7 +15,7 @@ window.tripConfig = {
     description: "把主要住宿、景點或集合地點放在這裡。放入 Google Maps API key 後會顯示互動地圖。",
     query: "Tokyo, Japan",
     zoom: 12,
-    googleMapsApiKey: "<iframe src="https://www.google.com/maps/d/embed?mid=1fHAk2A0xAFNs1Vj2IZfE380LtdWxj_k&ehbc=2E312F" width="640" height="480"></iframe>", // 在引號中貼上你的 Google Maps Embed API key。
+    googleMapsApiKey: "https://www.google.com/maps/d/embed?mid=1fHAk2A0xAFNs1Vj2IZfE380LtdWxj_k&ehbc=2E312F" width="640" height="480"", // 在引號中貼上你的 Google Maps Embed API key。
   },
 
   supabase: {
